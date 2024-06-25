799b8f6999e9b85d471409db391f3e8c29c762c8
jeremy woo <jeremy>
2024-06-26 06:16:24 +1200

    ncrontab examples support configurations.

---
+++ netcrontab/NcrontabExample/NcrontabExample/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var cron = NCrontab.CrontabSchedule.Parse("5 * * * *");
+var start  = DateTime.Now;
+var end = DateTime.Now.AddHours(1);
+
+var occurences =  cron.GetNextOccurrences(start, end);
+foreach (var occurence in occurences)
+{
+    Console.WriteLine(occurence);
+}
